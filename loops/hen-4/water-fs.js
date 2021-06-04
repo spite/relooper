142c528a5cf61e1115b4105169d9b61198fd5584
thespite <the.spite@gmail.com>
2021-06-04 23:08:54 +0100

    Update water-fs.js

--- loops/hen-4/water-fs.js
+++ loops/hen-4/water-fs.js
@@ -15,15 +15,15 @@ varying vec2 vUv;
 ${noise3d}
 
 float map( in vec3 p ){
-  vec3 q = .5*p;//p - vec3(0.0,0.1,1.0)*iTime;
+  vec3 q = p;//p - vec3(0.0,0.1,1.0)*iTime;
   float f;
-    f  = .5 + .5 * noise3d( q );
+    f  = 0.50000*noise3d( q );
     q = q*2.02;
-    f += .5 + .5 * noise3d( q );
+    f += 0.25000*noise3d( q );
   return f;
 }
 
-#define MAX 20
+#define MAX 200
 
 void main() {
   vec3 lightColor = vec3(41.,86.,80.)/255.;
@@ -39,26 +39,25 @@ void main() {
   dir = normalize(dir);
   float fSteps = 40. * d;
   int steps = int(fSteps);
-  vec3 fStep = normalize(dir);//dir / fSteps;
+  vec3 fStep = dir / fSteps;
   float n = 0.;
   vec3 p = back.xyz;
   for(int i=0; i<MAX; i++){
-    if( n>1.) {
+    if(i>steps || n>1.) {
       break;
     }
     p = front.xyz + float(i) * fStep;
-    float v = clamp(map(p),0.,1.);
-    n += v;///fSteps;
+    float v = clamp(map(1.5*p),0.,1.);
+    n += v/fSteps;
   }
-  // n *= 10.;
+  n *= 5.;
   vec3 color = texture2D(colorTexture, vUv).xyz;
   float mask = clamp(ld,0.,1.);
   gl_FragColor.rgb = mix(darkColor,lightColor*n,mask);
-  // gl_FragColor.rgb *= color.rgb;
-  // gl_FragColor.rgb += color * back.a;
-  // float rim = smoothstep(.5,1.,(1.-ld));
-  // gl_FragColor.rgb += lightColor *rim * back.a;
-  // gl_FragColor.rgb = mix(vec3(.3), gl_FragColor.rgb, back.a);
+  gl_FragColor.rgb += color * back.a;
+  float rim = smoothstep(.5,1.,(1.-ld));
+  gl_FragColor.rgb += lightColor *rim * back.a;
+  gl_FragColor.rgb = mix(vec3(.3), gl_FragColor.rgb, back.a);
   gl_FragColor.a = 1.;
 }`;
 
